af3bc3ae27d3410ad514bb764e9561c2df3f9ead
Alexandr Ivanov <ivanov-ay@gaz-is.ru>
2022-05-26 15:39:25 -0700

    Коммит содержит решение к модулю-0 и модулю-1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,8 @@
 function factorial(n) {
-  // ваш код...
+  if (n==0 || n ==1) return (+1)
+  let num = n
+  for (let index = 1; index < n; index++) {
+    num = num * (n - index)
+  }
+  return num
 }
