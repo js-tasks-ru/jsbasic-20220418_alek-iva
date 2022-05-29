eb03c957ab6c5b2c4e77d74f7a6f5b39360efb23
Alexandr Ivanov <ivanov-ay@gaz-is.ru>
2022-05-29 13:53:20 -0700

    Сделан 4 модуль

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let separated = str.split("-")
+
+  for (let i = 1; i < separated.length; i++) {
+    separated[i] = separated[i][0].toUpperCase() + separated[i].slice(1) 
+  }
+
+  return separated.join('')
 }
